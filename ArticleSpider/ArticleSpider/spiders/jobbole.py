4e230df4fcb551adfc01d15f6cbd0a5250937dbd
曾富田 <zftaife@163.com>
2017-09-14 18:18:00 +0800

    ss

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -5,7 +5,8 @@ import scrapy
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/112473/']
 
     def parse(self, response):
+        re_selector = response.xpath('//*[@id="post-112473"]/div[1]/h1')
         pass
